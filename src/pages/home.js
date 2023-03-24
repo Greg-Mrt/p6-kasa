01e538a73b1ed669846e147a23c205329bb06dde
Gregoire Martin <g.martin@langloys.com>
2023-03-24 16:34:44 +0100

    add footer on home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../components/header';
+import Footer from '../components/footer';
 
 const HomePage = () => {
     return (
       <div>
         <Header />
+
+        <Footer />
 
       </div>
     );
