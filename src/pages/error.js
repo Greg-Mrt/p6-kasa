e3ecc374c0564da5739365104c7befb015f8af76
Greg-Mrt <107677202+Greg-Mrt@users.noreply.github.com>
2023-03-25 16:35:39 +0100

    fixe error page

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/header";
 import './pages.css'
+import { Link } from 'react-router-dom';
+
 
 const ErrorPage = () => {
   return (
@@ -8,6 +10,10 @@ const ErrorPage = () => {
       <Header />
 
       <h1> 404 </h1>
+      <p className="subtitle">Oups! La page que vous demandez n'existe pas.</p>
+
+      <Link to="/home" className="homeLink">Retourner sur la page d’accueil</Link>
+
     </div>
   );
 };
