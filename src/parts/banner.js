eea697d0961d4d1f5b49951c66b6f92b1c433465
Greg-Mrt <107677202+Greg-Mrt@users.noreply.github.com>
2023-03-31 12:06:14 +0200

    few style changes

--- src/parts/banner.js
+++ src/parts/banner.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Banner({ page }) {
   let imageUrl = "";
+  let title = "";
 
   if (page === "HomePage") {
     imageUrl = process.env.PUBLIC_URL + "/images/ocean.png";
+    title = "Chez vous, partout et ailleurs"
   } else if (page === "AProposPage") {
     imageUrl = process.env.PUBLIC_URL + "/images/montagnes.png";
   } 
@@ -12,6 +14,7 @@ function Banner({ page }) {
   return (
     <div className="banner">
       <img className="img-banner" src={imageUrl} alt="Bannière" />
+      {page === "HomePage" && <h1 className="bannerTitle">{title}</h1>}
     </div>
   );
 }
